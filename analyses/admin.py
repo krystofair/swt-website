4729618f363d650bc040c8b70533d15617558448
Krzysztof Kłapyta <33362868+krystofair@users.noreply.github.com>
2025-02-15 15:34:53 +0100

    Delete olds.
    Deleted analyses and orders app, whom behaviuor is merged in kasbeer

--- analyses/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-
-# No tak tutaj można zdefiniować modele analiz w celach ograniczania dostępu konkretnym użytkownikom.
-
-admin.register(models.Analysis)
